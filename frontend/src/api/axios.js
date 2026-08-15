8f9d278ffa143ef33a0c5a795ed300a44e511aed
Rishen <rishenlap@gmail.com>
2026-08-15 15:50:38 +0545

    Fix for deployment

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // It will look for VITE_API_URL. If it doesn't find it (local dev), it falls back to localhost.
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
   headers: {
     "Content-Type": "application/json",
   },
 });
+
 
 // Attach token to every request
 api.interceptors.request.use((config) => {
